ad7e8880e480312574e644b99e04e7ffa3e25391
ProgrammingWizzard <programmingwizzard@gmail.com>
2017-05-19 20:32:44 +0200

    Create TokenCache

--- src/main/java/com/programmingwizzard/lvlup/token/TokenRefresher.java
+++ src/main/java/com/programmingwizzard/lvlup/token/TokenRefresher.java
@@ -1,6 +1,9 @@
 package com.programmingwizzard.lvlup.token;
 
 import com.programmingwizzard.lvlup.API;
+import com.programmingwizzard.lvlup.APIException;
+
+import java.util.concurrent.TimeUnit;
 
 public class TokenRefresher {
 
@@ -9,8 +12,10 @@ public class TokenRefresher {
     private final String login;
     private final String password;
 
-    // TODO: cache
-    private Token token;
+    // TODO: configuration
+    private final TokenCache cache = new TokenCache(15, TimeUnit.MINUTES);
+
+    private boolean present;
 
     public TokenRefresher(API api, String login, String password) {
         this.api = api;
@@ -19,12 +24,11 @@ public class TokenRefresher {
     }
 
     public Token refresh() {
-        // TODO: cache
-        return this.api.login();
+        return this.cache.get();
     }
 
     public boolean isPresent() {
-        return this.token != null;
+        return this.present;
     }
 
     public String getLogin() {
@@ -33,5 +37,64 @@ public class TokenRefresher {
 
     public String getPassword() {
         return password;
+    }
+
+    private class TokenCache {
+
+        private final int value;
+        private final TimeUnit unit;
+
+        private long expire;
+        private Token token;
+
+        public TokenCache(int value, TimeUnit unit) {
+            this.value = value;
+            this.unit = unit;
+        }
+
+        public Token get() {
+            if (System.currentTimeMillis() > expire) {
+                token = null;
+
+                if (present) {
+                    present = false;
+                }
+            }
+            if (expire == 0 || token == null) {
+                token = TokenRefresher.this.api.login();
+
+                this.expire = this.calc();
+                if (!present) {
+                    present = true;
+                }
+            }
+            return token;
+        }
+
+        private long calc() {
+            long time = System.currentTimeMillis();
+
+            switch (unit) {
+                case MILLISECONDS:
+                    time = time + value;
+                    break;
+                case SECONDS:
+                    time = time + value * 1000;
+                    break;
+                case MINUTES:
+                    time = time + (value * 1000) * 60;
+                    break;
+                case HOURS:
+                    time = time + ((value * 1000) * 60) * 60;
+                    break;
+                case DAYS:
+                    time = time + (((value * 1000) * 60) * 60) * 24;
+                    break;
+                default:
+                    throw new APIException("Not implementable yet!");
+            }
+            return time;
+        }
+
     }
 }
